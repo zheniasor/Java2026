68687cd8101eed999c798038e3e6ae6f1323f3ca
Evgenia <zheniasor@gmail.com>
2026-02-16 19:37:00 +0300

    Add first task

---
+++ helloWorld/demo/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/controller")
+public class Controller extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
